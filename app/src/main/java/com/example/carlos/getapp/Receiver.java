ca62870d70625411e5ef4ac5189005ac4f8e4f17
Unknown <cgc0045@alu.ubu.es>
2017-11-01 19:13:49 +0100

    Ringtone
    Hecho parte de la clase Ringtone. De momento esta puesto que suene una sola canción. Mejora en que se pueda elegir o entre varios tonos o en seleccionar una canción del móvil.

--- app/src/main/java/com/example/carlos/getapp/Receiver.java
+++ app/src/main/java/com/example/carlos/getapp/Receiver.java
@@ -1,16 +1,27 @@
 package com.example.carlos.getapp;
 
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**
  * Created by carlos on 1/11/17.
  */
 
 public class Receiver extends BroadcastReceiver {
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        Intent service = new Intent(context, Ringtone.class);
+
+        context.startService(service);
 
     }
 }
--- app/src/main/java/com/example/carlos/getapp/Ringtone.java
+++ app/src/main/java/com/example/carlos/getapp/Ringtone.java
@@ -1,8 +1,38 @@
 package com.example.carlos.getapp;
 
+import android.app.Service;
+import android.bluetooth.BluetoothClass;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
 /**
  * Created by carlos on 1/11/17.
  */
 
-public class Ringtone {
+public class Ringtone extends Service {
+    MediaPlayer song;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent){
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+
+        song = MediaPlayer.create(this, R.raw.entredosmares);
+        song.start();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy(){
+        Toast.makeText(this, "on destroy called", Toast.LENGTH_SHORT).show();
+    }
 }
